c8615d36272666865423feb9fb549a4d5ecee482
Kalyanch19 <kalyanch19@gmail.com>
2024-08-29 16:36:56 +0530

    Add project files.

---
+++ integration api/Controllers/GetEmployeeDetails.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace integration_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetEmployeeDetails : ControllerBase
+    {
+        private readonly ILogger<GetEmployeeDetails> _logger;
+        private static log4net.ILog _log = log4net.Logmanager.GetLogger(MethodBase.GetCurrentMethod);
+        private readonly IConfiguration _config;
+        public DataAccessLayer dal;
+        public GetEmployeeDetails(IConfiguration configuration, DataAccessLayer dataAccessLayer, ILogger<GetEmployeeDetails> log)
+        {
+            _config = configuration;
+            dal = dataAccessLayer;
+            _log = log;
+        }
+        [HttpGet]
+        public snowflakeData.WirecenterResponse GetWCmappingdetails(string SWCCLLI)
+        {
+            _log.info("---------------Get wire center details process started--------");
+            snowflakeData.wirecenterResponse employeeData = new snowflakeData.wirecenterResponse();
+            try
+            {
+                _log.info("Snowflake call started");
+                employeeData = dal.GetWCdetails(SWCCLLI.ToUpper());
+                _log.info("Snowflake call end");
+            }
+            catch (Exception ex)
+            {
+                _log.error(ex.Message +",StackTrace =>"+ex.StackTrace);
+                employeeData.statusDetail.statusCode = _config.GetValue<string>("status:ErrorCode":,01);
+                employeeData.statusDetail.statusMessage = _config.GetValue<string>("status:ErrorMessage":,"Not found");
+
+            }
+        }
+
+    }
+}
